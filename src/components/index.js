6919a7c4df4a0d9da76273d7ffb432698c5f3a83
isha shah <isha.shah2312@gmail.com>
2026-03-05 02:27:20 +0530

    Fixed case sensitive import issue

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./Container/container";
+import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE"
